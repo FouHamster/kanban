a920c8f62d34535a6177a511607e58ba47b721e6
FouHamster <alina63111@gmail.com>
2024-05-04 16:50:31 +0700

    done

--- js/main.js
+++ js/main.js
@@ -49,7 +49,8 @@ Vue.component('kanban', {
                 title: item.title,
                 description: item.description,
                 deadLine: item.deadLine,
-                createdDate: new Date()
+                createdDate: new Date(),
+                isFinishInTime: false
             };
             this.cards.push(cardItem);
         },
@@ -76,13 +77,16 @@ Vue.component('kanban', {
                 }
             }
         },
-        back(id) {
+        back(id, backDescription) {
             for (let i = 0; i < this.cards.length; i++) {
                 if (this.cards[i].id == id) {
                     let currentCard = this.cards[i];
                     // если карта во втором столбце переносим ее в 1
                     if (currentCard.status == 2)
+                    {
                         currentCard.status -= 1;
+                        currentCard.backDescription = backDescription;
+                    }
                 }
             }
         },
@@ -91,8 +95,27 @@ Vue.component('kanban', {
                 if (this.cards[i].id == id) {
                     let currentCard = this.cards[i];
                     // переносим карту в следующий столбец
+
                     if (currentCard.status < 3)
                         currentCard.status += 1;
+
+                    if (currentCard.status == 3) {
+                        let deadLineDate = new Date(currentCard.deadLine);
+                        let dYear = deadLineDate.getFullYear();
+                        let dMonth = deadLineDate.getMonth();
+                        let dDay = deadLineDate.getDate();
+
+                        let currentTime = new Date();
+                        let cYear = currentTime.getFullYear();
+                        let cMonth = currentTime.getMonth();
+                        let cDay = currentTime.getDate();
+
+                        if (dYear < cYear || dMonth < cMonth || dDay < cDay) {
+                            currentCard.isFinishInTime = false;
+                        } else {
+                            currentCard.isFinishInTime = true;
+                        }
+                    }
                 }
             }
         }
@@ -153,20 +176,23 @@ Vue.component('card', {
 
     template: `
     <div>
-        <div  v-if="!isEdit">
+        <div v-if="!isEdit && !isBack">
             <p>Заголовок: {{card.title}}</p>
             <p>Описание задачи: {{card.description}}</p>
             <p>Дата создаиния: {{card.createdDate.getFullYear()}}-{{card.createdDate.getMonth() + 1}}-{{card.createdDate.getDate()}}</p>
             <p>Дедлайн: {{card.deadLine}}</p>
+            <p v-if="card.backDescription">Причина возврата: {{card.backDescription}}</p>
             <p  v-if="card.editTime">Дата Редактирования: {{card.editTime.getFullYear()}}-{{card.editTime.getMonth() + 1}}-{{card.editTime.getDate()}}</p>
             <div class="card-actions" v-if="!(card.status == 3)">
-                <button v-if="!(card.status == 0)" @click="() => back(card.id)">Назад</button>
+                <button v-if="card.status == 2" @click="backActionOpen">Назад</button>
                 <button  @click="() => forword(card.id)">Вперед</button>
             </div>
             <div class="card-actions" v-if="!(card.status == 3)">
                 <button @click="editCurrentCard">Редактировать</button>
                 <button v-if="card.status == 0" @click="() => deleteCard(card.id)">Удалить</button>
             </div>
+            <h2 class="good" v-if="card.isFinishInTime">Выполнено в срок</h2>
+            <h2 class="bad" v-if="!card.isFinishInTime && card.status == 3">Выполнено не в срок</h2>
         </div>
         <div v-if="isEdit">
             <h2>Edit mode</h2>
@@ -178,6 +204,12 @@ Vue.component('card', {
             <input :value="deadLine" type="date" @input="event => deadLine = event.target.value"/>
             <button @click="saveEdit">Save</button>
         </div>
+        <div v-if="isBack">
+            <h2>Возврат</h2>
+            <p>Укажите причину возврата:</p>
+            <input type="text" @input="event => backDescription = event.target.value"/>
+            <button @click="backActionClose">Вернуть</button>
+        </div>
     </div>
     `,
 
@@ -187,6 +219,8 @@ Vue.component('card', {
             title: this.card.title,
             description: this.card.description,
             deadLine: this.card.deadLine,
+            isBack: false,
+            backDescription: "",
         }
     },
 
@@ -205,6 +239,13 @@ Vue.component('card', {
                 this.deadLine,
                 editTime
             );
+        },
+        backActionOpen() {
+            this.isBack = true;
+        },
+        backActionClose() {
+            this.back(this.card.id, this.backDescription);
+            this.isBack = false;
         }
     }
 })
